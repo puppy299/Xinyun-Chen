281efaa040aadaf7bd6945e31a34a2d10a6165e7
puppy299 <58747138+puppy299@users.noreply.github.com>
2019-12-10 20:22:13 +0000

    test

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace cw2.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Add test method
+            MainLogin class1 = new MainLogin();
+        }
+    }
+}
